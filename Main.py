bbe8dc6b1d7ce6aebe41c88d919b163b42fcc60d
Jhonny Mineu <104777793+JhonnyMineu@users.noreply.github.com>
2024-01-13 18:06:37 -0300

    Alterando layout da tela de login

--- Main.py
+++ Main.py
@@ -1,24 +1,55 @@
-import tkinter
+import customtkinter as ctk
+from tkinter import *
 
-janela = tkinter.Tk()
-janela.geometry("500x300")
+janela = ctk.CTk()
 
-def clique():
-    print("Teste")
-    
-texto = tkinter.Label(janela, text="Login")
-texto.pack(padx=10, pady=10)
+class Application():
+    def __init__(self):
+        self.janela = janela
+        self.tema()
+        self.tela()
+        self.janela_login()
+        janela.mainloop()
 
-email = tkinter.Entry(janela)
-email.pack(padx=10, pady=10)
+    def tema(self):
+        # Estilo da janela
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("green")
 
-senha = tkinter.Entry(janela, show="*")
-senha.pack(padx=10, pady=10)
+    def tela(self):
+        # Janela de login
+        janela.geometry("700x400")
+        janela.title("Controle MS Ao Vivo")
+        janela.resizable(width=FALSE,height=FALSE)
+        janela.iconbitmap("logo.ico")
 
-checkbox = tkinter.Checkbutton (janela, text="Mostrar senha?")
-checkbox.pack(padx=10, pady=10)
+    def janela_login(self):
+        # Imagem do bidu 
+        img = PhotoImage(file="C:/Users/jhow_/OneDrive/Documentos/GitHub/Tela-de-Login/bidu.png")
+        label_img = ctk.CTkLabel(janela, image=img, text="")
+        label_img.place(x=20,y=35)
 
-botao = tkinter.Button(janela, text="Entrar", command=clique)
-botao.pack(padx=10, pady=10)
+        # Parte direita da janela
+        frame = ctk.CTkFrame(janela, width=350, height=396)
+        frame.pack(side=RIGHT)
 
-janela.mainloop()
+        # Campos de input do usuário
+        texto = ctk.CTkLabel(frame, text="Login", font=('Roboto', 30, 'bold'), text_color= ('white'))
+        texto.place(x=140,y=60)
+
+        usuario = ctk.CTkEntry(frame, placeholder_text= "Seu usuário", width=300, font=('Roboto', 14)).place(x=25,y=155)
+
+        senha = ctk.CTkEntry(frame, placeholder_text= "Sua senha", show="*",
+                                        width=300, font=('Roboto', 14)).place(x=25,y=195)
+
+        checkbox = ctk.CTkCheckBox(frame, text="Mostrar senha?",  onvalue=1,
+                                            offvalue=0, width=50, height=15).place(x=205,y=235)
+
+        botao = ctk.CTkButton(frame, text="Entrar").place(x=110,y=280)
+
+
+
+Application()
+
+
+
